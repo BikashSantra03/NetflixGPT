2bf165d78795021f3dc171e9756ea00fce0b669b
BikashSantra03 <santrabikash922@gmail.com>
2025-03-12 11:44:56 +0530

    Custom Hook for   TMDB API call

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect } from "react";
-
 import Header from "../components/Header";
-import { API_OPTIONS } from "../../utils/constatnt";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../../utils/store/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-
-    const jsonData = await data.json();
-
-    dispatch(addNowPlayingMovies(jsonData.results));
-    console.log(jsonData);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
   return <Header inBrowsePage={true} />;
 };
 
