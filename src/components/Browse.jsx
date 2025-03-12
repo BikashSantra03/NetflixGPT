b82e441b820e4d17bca09b30c472014ba91093ca
BikashSantra03 <santrabikash922@gmail.com>
2025-03-12 10:40:44 +0530

    Getting Data from TMDB API

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Header from "../components/Header";
+import { API_OPTIONS } from "../../utils/constatnt";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS
+    );
+
+    const jsonData = await data.json();
+    console.log(jsonData);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
   return <Header inBrowsePage={true} />;
 };
 
