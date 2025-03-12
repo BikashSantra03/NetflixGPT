27f9393ad865e6a07a6342f963cb178b9f84a2c4
BikashSantra03 <santrabikash922@gmail.com>
2025-03-12 11:35:48 +0530

    Adding Movie Data to my Store (MovieSlice)

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -2,8 +2,12 @@ import React, { useEffect } from "react";
 
 import Header from "../components/Header";
 import { API_OPTIONS } from "../../utils/constatnt";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../../utils/store/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -11,6 +15,8 @@ const Browse = () => {
     );
 
     const jsonData = await data.json();
+
+    dispatch(addNowPlayingMovies(jsonData.results));
     console.log(jsonData);
   };
 
